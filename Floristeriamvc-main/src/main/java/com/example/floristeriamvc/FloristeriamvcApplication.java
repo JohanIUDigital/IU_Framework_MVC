75509be9f6ef93ddad4c0c8fcfbdc79d1069f40a
AlexisHolguinJDL <alexis.holguin@jdlsas.com>
2024-11-20 20:55:30 -0500

    si se puede

--- Floristeriamvc-main/src/main/java/com/example/floristeriamvc/FloristeriamvcApplication.java
+++ Floristeriamvc-main/src/main/java/com/example/floristeriamvc/FloristeriamvcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FloristeriamvcApplication {
 
     public static void main(String[] args) {
-        System.out.println("Aplicacion ready para correr");
+        System.out.println("Aplicacion ready para correr 20:53");
         SpringApplication.run(FloristeriamvcApplication.class, args);
     }
 }
